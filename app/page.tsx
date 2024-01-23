fdb29cd308a4c871259ff6a95b8ccb9dae11e955
Koala5617 <joaogchs5789@gmail.com>
2024-01-23 15:50:40 -0300

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,62 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import styles from "./home.module.css";
+import NationPreview from "./ui/NationPreview";
+import {Search, RegionSelect} from "./ui/filterRow";
 
-export default function Home() {
+type Nation = {
+  name: string, 
+  capital:string, 
+  region:string, 
+  flag:string, 
+  flagAlt:string, 
+  population:string
+}
+
+
+function cards(countries:Nation[]) {
+  return countries.map((country:Nation) => NationPreview(country))
+}
+
+
+export default async function Home() {
+  
+  const info = await fetch(`https://restcountries.com/v3.1/independent?status=true&fields=name,cca3,flags,population,region,capital`)
+  const countries = await info.json()
+  var cardInfo: Nation[] = []
+  
+  // gets the informations for the cards
+  for (let index = 0; index < countries.length; index++) {
+    let countryinfo: Nation = {
+      'name': countries[index].name.common,
+      'capital': countries[index].capital[0], 
+      'region': countries[index].region,
+      'flag': countries[index].flags.svg,
+      'flagAlt': countries[index].flags.alt,
+      'population': Intl.NumberFormat('en-US').format(countries[index].population)}; 
+    cardInfo.push(countryinfo);
+  }
+
+  // organizes the items alphabetically by name
+  cardInfo.sort(function (a:any, b:any) {
+    if (a.name > b.name) {
+      return 1;
+    }
+    if (a.name < b.name) {
+      return -1;
+    }
+    return 0;
+  })
+  
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+      <div className={styles.filterRow}>
+        <Search />
+        <RegionSelect />
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
       <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        {cards(cardInfo) /*generates each nation's card*/}
       </div>
     </main>
   );
 }
+
---
+++ app/ui/tooltip.tsx
@@ -0,0 +1,19 @@
+import styles from "../[detail]/detail.module.css"
+
+function innerTooltip(input:string[]) {
+    if (input.length == 1) {
+        return
+    }
+    return input.map((input:string) => <>{input}<br /></> )
+  }
+export default function Tooltip({text, tooltipItems}:{text:string, tooltipItems:string[]}) {
+    return (
+    <>
+    <span className={`${styles.value} ${styles.tooltipText}`}>
+        {text}
+        <span className={styles.tooltipBox}>
+        {innerTooltip(tooltipItems)}
+        </span>
+    </span>
+    </>)
+}
